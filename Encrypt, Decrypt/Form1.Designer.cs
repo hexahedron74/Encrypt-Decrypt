71430fc35a48f3189ba3b7ef9a9fb1792c88bd0d
Hexahedron74 <ithan0704@gmail.com>
2021-05-02 01:37:42 +0900

    초기 커밋.

---
+++ Encrypt, Decrypt/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Encrypt__Decrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtValue = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtEncrypt = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtDecrypt = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValue
+            // 
+            this.txtValue.BackColor = System.Drawing.Color.White;
+            this.txtValue.Depth = 0;
+            this.txtValue.Hint = "";
+            this.txtValue.Location = new System.Drawing.Point(116, 98);
+            this.txtValue.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtValue.Name = "txtValue";
+            this.txtValue.PasswordChar = '\0';
+            this.txtValue.SelectedText = "";
+            this.txtValue.SelectionLength = 0;
+            this.txtValue.SelectionStart = 0;
+            this.txtValue.Size = new System.Drawing.Size(446, 23);
+            this.txtValue.TabIndex = 0;
+            this.txtValue.UseSystemPasswordChar = false;
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.BackColor = System.Drawing.Color.White;
+            this.txtEncrypt.Depth = 0;
+            this.txtEncrypt.Hint = "";
+            this.txtEncrypt.Location = new System.Drawing.Point(116, 152);
+            this.txtEncrypt.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.PasswordChar = '\0';
+            this.txtEncrypt.SelectedText = "";
+            this.txtEncrypt.SelectionLength = 0;
+            this.txtEncrypt.SelectionStart = 0;
+            this.txtEncrypt.Size = new System.Drawing.Size(446, 23);
+            this.txtEncrypt.TabIndex = 1;
+            this.txtEncrypt.UseSystemPasswordChar = false;
+            // 
+            // txtDecrypt
+            // 
+            this.txtDecrypt.BackColor = System.Drawing.Color.White;
+            this.txtDecrypt.Depth = 0;
+            this.txtDecrypt.Hint = "";
+            this.txtDecrypt.Location = new System.Drawing.Point(116, 203);
+            this.txtDecrypt.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtDecrypt.Name = "txtDecrypt";
+            this.txtDecrypt.PasswordChar = '\0';
+            this.txtDecrypt.SelectedText = "";
+            this.txtDecrypt.SelectionLength = 0;
+            this.txtDecrypt.SelectionStart = 0;
+            this.txtDecrypt.Size = new System.Drawing.Size(446, 23);
+            this.txtDecrypt.TabIndex = 2;
+            this.txtDecrypt.UseSystemPasswordChar = false;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.BackColor = System.Drawing.Color.White;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(43, 102);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(55, 19);
+            this.materialLabel1.TabIndex = 3;
+            this.materialLabel1.Text = "Value :";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.BackColor = System.Drawing.Color.White;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(31, 156);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(67, 19);
+            this.materialLabel2.TabIndex = 4;
+            this.materialLabel2.Text = "Encrypt :";
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.BackColor = System.Drawing.Color.White;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(31, 207);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(68, 19);
+            this.materialLabel3.TabIndex = 5;
+            this.materialLabel3.Text = "Decrypt :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(338, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "ENCRYPT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(464, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "DECRYPT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(583, 303);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.txtDecrypt);
+            this.Controls.Add(this.txtEncrypt);
+            this.Controls.Add(this.txtValue);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encrypt, Decrypt Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtValue;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtEncrypt;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtDecrypt;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Encrypt, Decrypt/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encrypt__Decrypt
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        String hash = "hex@hedr0n"; //여러분들이 적고 싶은 거 적으시면 됩니다.
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] data = UTF8Encoding.UTF8.GetBytes(txtValue.Text);
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                    using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                    {
+                        ICryptoTransform transform = tripDes.CreateEncryptor();
+                        byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                        txtEncrypt.Text = Convert.ToBase64String(results, 0, results.Length);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + "디코드 할 문자열을 다시 한번 확인해주시기 바랍니다.", "Error(ISD01)", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] data = Convert.FromBase64String(txtEncrypt.Text);
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                    {
+                        byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                        using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                            {
+                                ICryptoTransform transform = tripDes.CreateDecryptor();
+                                byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                                txtDecrypt.Text = UTF8Encoding.UTF8.GetString(results);
+                             }
+                    }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + "디코드 할 문자열을 다시 한번 확인해주시기 바랍니다.", "Error(ISD01)", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
